519d93a95921075ad520306c2b80a0982267f873
CassartArnaud <etu38805@henallux.be>
2020-06-04 19:03:57 +0200

    génération des patterns terminé

--- classificationStatistics.c
+++ classificationStatistics.c
@@ -12,37 +12,6 @@
 #define LINE printf("\n");
 
 
-/* Contiens un tableau de classes et la taille de ce tableau
- *
- * vector est un tableau contenant des entiers
- * size est la taille de ce tableau
- *
- */
-/*
-typedef struct Classes Classes;
-struct Classes {
-    int *vector;
-    int size;
-};*/
-/* Prends le tableau oldArray et le copie dans un nouveau tableau qui mesure une unité de plus que oldArray
- *
- * @param oldArray ancien tableau que l'on souhaite agrandir et copier
- * @param newArray nouveau tableau de taille sizeOldArray +1 dans lequel on va copier les données de l'ancien
- * @param sizeOldArray taille de l'ancien tableau oldArray
- *
- *//*
-void sizeUpArray(int* oldArray,int* newArray, int sizeOldArray){
-    for(int i=0;i<sizeOldArray;i++)
-        newArray[i] = oldArray[i];
-}*/
-
-/* Ajoute à la au tableau classes un élément si celui-ci n'existe pas déjà dedans. Cette fonction incrémentera ensuite le compteur de classes de 1 si elle à ajouté un élément
- *
- * @param classes est un pointeur vers un tableau tableau et nbClasses est la taille de celui-ci. Il est utilisé pour stocker les différentes classes individuellement différentes
- * @param realClasse est le nouvel élément dont on doit vérifier l'existence *
- * @return la structure classes entrée en paramètre d'entrée à laquelle on doit ou non ajouter newItem si celui-ci n'était pas déjà présent. cette structure aura son compteur size augmenté de 1 si un élément à été ajouté
- * */
-
 int addWithoutDuplicateClasses(int* classes,int realClasse, int nbClasse)
 {
 
@@ -66,7 +35,6 @@ int addWithoutDuplicateClasses(int* classes,int realClasse, int nbClasse)
 }
 
 
-
 /* 	Crée un tableau reprenant les classes différentes que contiens realClasses et supprime les doublons.
  *
  *  @param realClasses est un tableau contenant de les classes existantes
@@ -84,40 +52,7 @@ int researchClasses(int* classes,int* realClasses, int realClassesSize)
     return nbClasse;
 }
 
-/* retourne le calcul d'un pourcentage de ( sum / total ) * 100
- *
- * @param sum est le nombre de réalisation
- * @param total est le nombre d'essais
- *
- * @return un pourcentage. Si sum est suppérieur à total, retourne 100.
- *//*
-double percentage(int sum, int total){
-    if (sum < total){
-        return ((double)sum/(double)total)*100;
-    }else{
-        return 100;
-    }
-}*/
 
-/* Test de la fonction de pourcentage
- * Cette méthode effectue des tests pour vérifier que la fonction pourcentage fonctionne toujours
- */
-/*int test_percentage(void){
-    int error_counter = 0;
-    int* error_counter_ptr= &error_counter;
-    error_assert(percentage(3300,10000) == 33.00,"simple de pourcentage",error_counter_ptr);
-    error_assert(percentage(3333,10000) == 33.33,"pourcentage avec 2 nombres derrière la virgule ",error_counter_ptr);
-    error_assert(percentage(1000,10) == 100,"sum suppérieur à total",error_counter_ptr);
-    return error_counter;
-}*/
-
-/*	Compare 2 entiers et vérifie que ceux-ci sont identiques
- *
- *	@param firstElement premier entier à comparer
- *	@param secondElement second entier à comparer
- *
- *	@return 0 si les 2 éléments sont identiques et autre chose sinon
- */
 void compare(int* malClasse,int* realClasses,int* estimateClasses,int nbRealClasse)
 {
     for (int i =0 ; i<nbRealClasse; i++)
@@ -167,17 +102,7 @@ void generateClasses(double** classes, int* realClasses, int* estimateClasses, d
     return error_counter;
 }*/
 
-/*	Compare 2 vecteurs entre eux et retourne un tableau de taille identique au premier vecteur mis en paramètre. Si l'élément du tableau vaut 0, cela signifie que les 2 vecteurs ont le même contenu.
- *
- *	@param realClasses est le premier vecteur a comparer
- *  @param estimateClasses est un second vecteur qui sera comparé au premier
- *
- *  @return un tableau d'entier de même taille que realClasses qui contiens un 0 quand les 2 éléments du tableau sont identiques et autre chose quand ils sont différents
- */
-/*int *compareVectors(int *realClasses, int *estimateClasses){
-    //int out[1];
-    return realClasses;
-}*/
+
 
 /*  Affiche un tableau reprenant les différentes classes disponibles (cfr interface1 - document de projet), combien ont bien été classées dans estimateClasses, le nombre d'occurences de chaque classe dans vecteur realClasses et un pourcentage de classes qui ont bien été classées dans estimateClasses.
  *
@@ -299,7 +224,7 @@ void displayBarChart(int* realClasses, int* estimateClasses, int nbRealClasse)
         LINE;
     }
 }
-void test_display()
+/*void test_display()
 {
     int testClasses[MAX_NB_PATTERN];// contient le nombre d'élément de chacune des classes
     char** category=(char**) calloc(4,sizeof(char*));
@@ -331,3 +256,4 @@ void test_classificationStatistics(void){
     printf("\tx Fonction display \n"); test_display();
     printf("Fin - Test de classification Statistics :\n");
 }
+ */
